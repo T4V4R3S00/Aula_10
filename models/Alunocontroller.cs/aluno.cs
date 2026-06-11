bc3480fc5f209d87b51f7890fba449d917175502
T4V4R3S00 <taveres.tiago1605@gmail.com>
2026-06-10 21:10:56 -0300

    formulario_pagina

---
+++ models/Alunocontroller.cs/aluno.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using MeuPrimeiroMVC.Models;
+
+namespace MeuPrimeiroMVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Confirmacao", aluno);
+            }
+
+            return View(aluno);
+        }
+
+        [HttpGet]
+        public IActionResult Confirmacao(Aluno aluno)
+        {
+            return View(aluno);
+        }
+    }
+}
